f843d6cebba506a1205857589800fc1af5ed59d3
Darragh <darragh1992@gmail.com>
2016-07-28 09:09:51 +0100

    debug =false

--- StaticHeroku/settings.py
+++ StaticHeroku/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't#y*qbf#*yl_+@197pqefvy_tdsyqh3_uq(569z*pd32fel%(k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
